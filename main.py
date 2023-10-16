bb71a347bd73e8a395216e7f78ef17aac6287768
Uriel Campos <urieldecampos@gmail.com>
2023-10-16 15:47:42 -0300

    Versao mais atual - Modificar este arquivo.

--- main.py
+++ main.py
@@ -1,33 +1,92 @@
 import pygame
 import random
-import sys
 import time
 
 pygame.init()
 pygame.display.set_caption("Jogo da cobrinha")
-largura, altura = 1000, 800
+largura, altura = 1000, 700
 tela = pygame.display.set_mode((largura, altura))
 relogio = pygame.time.Clock()
+fonte = pygame.font.Font(None, 36)
 
-#cores
+# cores
 azul = (0, 0, 255)
 branca = (255, 255, 255)
 vermelho = (255, 0, 0)
 verde = (0, 255, 0)
+rosa = (255, 105, 180)
+preta = (0, 0, 0)
 
-#parametros da cobrinha
-cabeca_cobra = pygame.image.load("./imagens/pixil-frame-0.png")
-corpo_cobra = pygame.image.load("./imagens/pixil-frame-0 (1).png")
-cauda_cobra = pygame.image.load("./imagens/pixil-frame-0 (3).png")
-comida = pygame.image.load("./imagens/pixil-frame-0 (2).png")
-raio = pygame.transform.scale(pygame.image.load("./imagens/raio.png"), (30, 30))
+
+# parametros da cobrinha
+cabeca_cobra = pygame.image.load("C:\\Snake-Game\\imagens\\pixil-frame-0.png")
+corpo_cobra = pygame.image.load("C:\\Snake-Game\\imagens\\pixil-frame-0 (1).png")
+cauda_cobra = pygame.image.load("C:\\Snake-Game\\imagens\\pixil-frame-0 (3).png")
+comida = pygame.image.load("C:\\Snake-Game\\imagens\\nemo.png")
+velocidade_inicial = 22
+velocidade_atual = 5
 tamanho_quadrado = 25
 
-velocidade_jogo = 20
+
+def tela_inicial():
+    texto_titulo = fonte.render("Jogo da Cobrinha", True, rosa)
+    tela.blit(texto_titulo, (largura // 2 - texto_titulo.get_width() // 2, 100))
+    # Instruções
+    texto_titulo = fonte.render("Use WASD para mover a Cobrinha", True, azul)
+    tela.blit(texto_titulo, (largura // 2 - texto_titulo.get_width() // 2, 150))
+    texto_titulo = fonte.render("Coma as frutinhas e fuja das paredes e de você mesmo", True, azul)
+    tela.blit(texto_titulo, (largura // 2 - texto_titulo.get_width() // 2, 200))
+    texto_titulo = fonte.render("Lembrando que o raio te deixa mais rápido. Use com cautela!", True, azul)
+    tela.blit(texto_titulo, (largura // 2 - texto_titulo.get_width() // 2, 250))
+    texto_instrucoes = fonte.render("Pressione ESPAÇO para começar", True, branca)
+    tela.blit(texto_instrucoes, (largura // 2 - texto_instrucoes.get_width() // 2, 500))
+
+    pygame.display.update()
+
+    aguardando_inicio = True
+    while aguardando_inicio:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if evento.type == pygame.KEYDOWN and evento.key == pygame.K_SPACE:
+                aguardando_inicio = False
+
+
+def tela_final():  # problemas: só funciona uma vez. Quando clica pra jogar de novo volta na velocidade que tava antes. Precisa colocar a pontuação
+    tela.fill(preta)
+    texto_titulo = fonte.render("Game Over", True, rosa)
+    tela.blit(texto_titulo, (largura // 2 - texto_titulo.get_width() // 2, 100))
+    # Novo jogo
+    jogar_de_novo = fonte.render("Pressione ESPAÇO para jogar novamente", True, azul)
+    tela.blit(jogar_de_novo, (largura // 2 - texto_titulo.get_width() // 2, 150))
+    # Sair do jogo
+    sair_jogo = fonte.render("Pressione ESC para sair do jogo", True, azul)
+    tela.blit(sair_jogo, (largura // 2 - texto_titulo.get_width() // 2, 200))
+
+    pygame.display.update()
+
+    aguardando_escolha = True
+    while aguardando_escolha:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_SPACE:
+                    aguardando_escolha = False
+                    # O jogador escolheu jogar novamente
+                    rodar_jogo()
+                elif evento.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
+
 
 def gerar_comida():
-    posicao_x_comida = round(random.randrange(40, largura-tamanho_quadrado+15) / float(tamanho_quadrado)) * float(tamanho_quadrado)
-    posicao_y_comida = round(random.randrange(40, altura-tamanho_quadrado+15) / float(tamanho_quadrado)) * float(tamanho_quadrado)
+    posicao_x_comida = round(random.randrange(40, largura - tamanho_quadrado + 15) / float(tamanho_quadrado)) * float(
+        tamanho_quadrado)
+    posicao_y_comida = round(random.randrange(40, altura - tamanho_quadrado + 15) / float(tamanho_quadrado)) * float(
+        tamanho_quadrado)
     return posicao_x_comida, posicao_y_comida
 
 def gerar_boost():
@@ -35,22 +94,21 @@ def gerar_boost():
     posicao_y_boost = round(random.randrange(40, altura-tamanho_quadrado+15) / float(tamanho_quadrado)) * float(tamanho_quadrado)
     return posicao_x_boost, posicao_y_boost
 
-
 def desenhar_cobra(pixels, corpo_cobra):
     tela.blit(cauda_cobra, [pixels[0][0], pixels[0][1]])
     pixels_invertidos = pixels[::-1]
-    #desenha o corpo da cobra
+    # desenha o corpo da cobra
     for pixel in pixels_invertidos[1:]:
         tela.blit(corpo_cobra, [pixel[0], pixel[1]])
-    #desenha a cabeça da cobra por último
+    # desenha a cabeça da cobra por último
     tela.blit(cabeca_cobra, [pixels_invertidos[0][0], pixels_invertidos[0][1]])
 
 
 def desenhar_comida(comida,posicao_x_comida,posicao_y_comida):
-        tela.blit(comida, [posicao_x_comida, posicao_y_comida])
+    tela.blit(comida, [posicao_x_comida, posicao_y_comida])
 
 def desenhar_boost(boost,posicao_x_boost,posicao_y_boost):
-        tela.blit(boost, [posicao_x_boost, posicao_y_boost])
+    tela.blit(boost, [posicao_x_boost, posicao_y_boost])
 
 
 def selecionar_velocidade(tecla):
@@ -74,11 +132,12 @@ def selecionar_velocidade(tecla):
 def desenhar_pontuacao(pontuacao):
     fonte = pygame.font.SysFont("Helvetica", 35)
     texto = fonte.render(f"Pontos: {pontuacao}", True, vermelho)
-    tela.blit(texto, [1,1])
+    tela.blit(texto, [1, 1])
 
-    
+
 def rodar_jogo():
     fim_jogo = False
+    global velocidade_atual
 
     global velocidade_jogo
 
@@ -98,14 +157,15 @@ def rodar_jogo():
     posicao_x_comida,posicao_y_comida = gerar_comida()
     posicao_x_boost, posicao_y_boost = gerar_boost()
 
+
     if x == posicao_x_comida and y == posicao_y_comida:
-        #adicione a posição da comida ao corpo da cobra
+        # adicione a posição da comida ao corpo da cobra
         pixels.append([posicao_x_comida, posicao_y_comida])
-        #gere uma nova posição para a comida
+        # gere uma nova posição para a comida
         posicao_x_comida, posicao_y_comida = gerar_comida()
-        #não aumente o tamanho_cobra, pois apenas o corpo deve crescer
+        # não aumente o tamanho_cobra, pois apenas o corpo deve crescer
     else:
-        #se não comeu comida, remova o último segmento do corpo para manter o tamanho constante
+        # se não comeu comida, remova o último segmento do corpo para manter o tamanho constante
         pixels.pop(0)
 
     while not fim_jogo:
@@ -121,7 +181,7 @@ def rodar_jogo():
             elif evento.type == pygame.KEYDOWN:
                 velocidade_x, velocidade_y = selecionar_velocidade(evento.key)
 
-        #desenhar comida
+        # desenhar comida
         desenhar_comida(comida, posicao_x_comida, posicao_y_comida)
 
         #desenhar boost
@@ -134,23 +194,23 @@ def rodar_jogo():
         x += velocidade_x
         y += velocidade_y
 
-        #desenhar cobra
+        # desenhar cobra
         pixels.append([x, y])
         if len(pixels) > tamanho_cobra:
             del pixels[0]
 
         for pixel in pixels[:-1]:
-            if pixel == [x,y]:
+            if pixel == [x, y]:
                 fim_jogo = True
         desenhar_cobra(pixels, corpo_cobra)
 
-        #desenhar pontuação
+        # desenhar pontuação
         desenhar_pontuacao(tamanho_cobra - 1)
 
-        #atualização da tela
+        # atualização da tela
         pygame.display.update()
 
-        #criar uma nova comida
+        # criar uma nova comida
         if x == posicao_x_comida and y == posicao_y_comida:
             tamanho_cobra += 1
             boost += 1
@@ -163,4 +223,7 @@ def rodar_jogo():
         #tempo de jogo
         relogio.tick(velocidade_jogo)
 
+
+tela_inicial()
 rodar_jogo()
+tela_final()
