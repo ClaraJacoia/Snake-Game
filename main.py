986a6f72ac98a0d3b037f7620faa0e6408d8d386
Uriel Campos <urieldecampos@gmail.com>
2023-10-16 15:25:59 -0300

    mecanica de boost pronta

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 import random
+import sys
+import time
 
 pygame.init()
 pygame.display.set_caption("Jogo da cobrinha")
-largura, altura = 1600, 800
+largura, altura = 1000, 800
 tela = pygame.display.set_mode((largura, altura))
 relogio = pygame.time.Clock()
 
@@ -14,17 +16,26 @@ vermelho = (255, 0, 0)
 verde = (0, 255, 0)
 
 #parametros da cobrinha
-cabeca_cobra = pygame.image.load("C:\\JogoCobrinha2\\pixil-frame-0.png")
-corpo_cobra = pygame.image.load("C:\\JogoCobrinha2\pixil-frame-0 (1).png")
-cauda_cobra = pygame.image.load("C:\\JogoCobrinha2\\pixil-frame-0 (3).png")
-comida = pygame.image.load("C:\\JogoCobrinha2\\pixil-frame-0 (2).png")
-velocidade_jogo = 22
+cabeca_cobra = pygame.image.load("./imagens/pixil-frame-0.png")
+corpo_cobra = pygame.image.load("./imagens/pixil-frame-0 (1).png")
+cauda_cobra = pygame.image.load("./imagens/pixil-frame-0 (3).png")
+comida = pygame.image.load("./imagens/pixil-frame-0 (2).png")
+raio = pygame.transform.scale(pygame.image.load("./imagens/raio.png"), (30, 30))
 tamanho_quadrado = 25
+
+velocidade_jogo = 20
 
 def gerar_comida():
     posicao_x_comida = round(random.randrange(40, largura-tamanho_quadrado+15) / float(tamanho_quadrado)) * float(tamanho_quadrado)
     posicao_y_comida = round(random.randrange(40, altura-tamanho_quadrado+15) / float(tamanho_quadrado)) * float(tamanho_quadrado)
     return posicao_x_comida, posicao_y_comida
+
+def gerar_boost():
+    posicao_x_boost = round(random.randrange(40, largura-tamanho_quadrado+15) / float(tamanho_quadrado)) * float(tamanho_quadrado)
+    posicao_y_boost = round(random.randrange(40, altura-tamanho_quadrado+15) / float(tamanho_quadrado)) * float(tamanho_quadrado)
+    return posicao_x_boost, posicao_y_boost
+
+
 def desenhar_cobra(pixels, corpo_cobra):
     tela.blit(cauda_cobra, [pixels[0][0], pixels[0][1]])
     pixels_invertidos = pixels[::-1]
@@ -33,8 +44,15 @@ def desenhar_cobra(pixels, corpo_cobra):
         tela.blit(corpo_cobra, [pixel[0], pixel[1]])
     #desenha a cabeça da cobra por último
     tela.blit(cabeca_cobra, [pixels_invertidos[0][0], pixels_invertidos[0][1]])
+
+
 def desenhar_comida(comida,posicao_x_comida,posicao_y_comida):
         tela.blit(comida, [posicao_x_comida, posicao_y_comida])
+
+def desenhar_boost(boost,posicao_x_boost,posicao_y_boost):
+        tela.blit(boost, [posicao_x_boost, posicao_y_boost])
+
+
 def selecionar_velocidade(tecla):
     if tecla == pygame.K_s:
         posicao_atual = "Baixo"
@@ -52,24 +70,33 @@ def selecionar_velocidade(tecla):
         velocidade_x = -tamanho_quadrado
         velocidade_y = 0
     return velocidade_x, velocidade_y
+
 def desenhar_pontuacao(pontuacao):
     fonte = pygame.font.SysFont("Helvetica", 35)
     texto = fonte.render(f"Pontos: {pontuacao}", True, vermelho)
     tela.blit(texto, [1,1])
+
+    
 def rodar_jogo():
     fim_jogo = False
+
+    global velocidade_jogo
 
     x = largura/2
     y = altura/2
 
+
     velocidade_x = 0
     velocidade_y = 0
+
+    boost = 1
 
     tamanho_cobra = 1
 
     pixels = [[largura / 2, altura / 2], [largura / 2 - tamanho_quadrado, altura / 2]]
 
     posicao_x_comida,posicao_y_comida = gerar_comida()
+    posicao_x_boost, posicao_y_boost = gerar_boost()
 
     if x == posicao_x_comida and y == posicao_y_comida:
         #adicione a posição da comida ao corpo da cobra
@@ -84,6 +111,10 @@ def rodar_jogo():
     while not fim_jogo:
         tela.fill(azul)
 
+        tempo_inicial = time.time()
+
+        duracao = 10
+
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 fim_jogo = True
@@ -92,6 +123,10 @@ def rodar_jogo():
 
         #desenhar comida
         desenhar_comida(comida, posicao_x_comida, posicao_y_comida)
+
+        #desenhar boost
+        if boost % 3 == 0:
+            desenhar_boost(raio, posicao_x_boost, posicao_y_boost)
 
         #atualizar a posição da cobra
         if x < 0 or x >= largura or y < 0 or y >= altura:
@@ -118,7 +153,12 @@ def rodar_jogo():
         #criar uma nova comida
         if x == posicao_x_comida and y == posicao_y_comida:
             tamanho_cobra += 1
+            boost += 1
             posicao_x_comida, posicao_y_comida = gerar_comida()
+
+        if x == posicao_x_boost and y == posicao_y_boost:
+            posicao_x_boost, posicao_y_boost = gerar_boost()
+            boost += 1
 
         #tempo de jogo
         relogio.tick(velocidade_jogo)
